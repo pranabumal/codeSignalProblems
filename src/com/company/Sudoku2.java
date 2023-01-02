9b43f2226fde4ca76ff3e0e5f090be26e1df4714
pranab <sarker.pranab@genweb2.com>
2023-01-02 15:49:09 +0600

    Soduku

--- src/com/company/Sudoku2.java
+++ src/com/company/Sudoku2.java
@@ -7,11 +7,13 @@ public class Sudoku2 {
 
     boolean solution(char[][] board) {
         boolean sodoku = true;
-
+//        Set<String> boxSet = new HashSet<>();
         for (int i = 0; board.length > i; i++) {
             Set<String> rowSet = new HashSet<>();
             Set<String> colSet = new HashSet<>();
+
             for (int j = 0; board[i].length > j; j++) {
+
                 if('.' != (board[j][i]) && colSet.contains(String.valueOf(board[j][i]))){
                     return false;
                 }else {
@@ -22,10 +24,29 @@ public class Sudoku2 {
                 } else {
                     rowSet.add(String.valueOf(board[i][j]));
                 }
+
+            }
+
+        }
+        //now the 3rd condition checking repetitions in 3x3 sub-boxes.
+        for(int i=0;i<9;i+=3){
+            for(int j=0;j<9;j+=3){
+                if(!check3(i,j,board)) return false;
             }
         }
-
-
+        
         return sodoku;
+    }
+    public boolean check3(int i,int j,char[][] board){
+        HashSet<Character> set3 = new HashSet<>();
+        for(int m=i;m<3+i;m++){
+            for(int n=j;n<3+j;n++){
+                if(board[m][n]=='.') continue;
+                else {
+                    if(!set3.add(board[m][n])) return false;
+                }
+            }
+        }
+        return true;
     }
 }
