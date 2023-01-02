ff4ac5401965ccec25eca4bef1fbceac915505da
pranabsarker <sarker.pranab@genweb2.com>
2023-01-02 10:21:32 +0600

    soduku

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
         String string = "ngrhhqbhnsipkcoqjyviikvxbxyphsnjpdxkhtadltsuxbfbrkof";
-        int [] array = new int[]{
-          2, 3, 2, 5, 3, 2, 4, 6, 6
+        int[] array = new int[]{
+                2, 3, 2, 5, 3, 2, 4, 6, 6
         };
-        int [][] multiDimensionArray = new int[][]{
+        int[][] multiDimensionArray = new int[][]{
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        RotateImage rotateImage = new RotateImage();
-        System.out.println((Arrays.deepToString(rotateImage.solution(multiDimensionArray))));
+        char[][] grid = new char[][]{
+                {'.', '.', '.', '1', '4', '.', '.', '2', '.'},
+                {'.', '.', '6', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '1', '.', '.', '.', '.', '.', '.'},
+                {'.', '6', '7', '.', '.', '.', '.', '.', '9'},
+                {'.', '.', '.', '.', '.', '.', '8', '1', '.'},
+                {'.', '3', '.', '.', '.', '.', '.', '.', '6'},
+                {'.', '.', '.', '.', '.', '7', '.', '.', '.'},
+                {'.', '.', '.', '5', '.', '.', '.', '7', '.'}};
+
+        Sudoku2 sudoku2 = new Sudoku2();
+        System.out.println(sudoku2.solution(grid));
     }
 
 }
--- src/com/company/Sudoku2.java
+++ src/com/company/Sudoku2.java
@@ -1,10 +1,31 @@
 package com.company;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Sudoku2 {
 
-    boolean solution(char[][] grid) {
+    boolean solution(char[][] board) {
+        boolean sodoku = true;
+
+        for (int i = 0; board.length > i; i++) {
+            Set<String> rowSet = new HashSet<>();
+            Set<String> colSet = new HashSet<>();
+            for (int j = 0; board[i].length > j; j++) {
+                if('.' != (board[j][i]) && colSet.contains(String.valueOf(board[j][i]))){
+                    return false;
+                }else {
+                    colSet.add(String.valueOf(board[j][i]));
+                }
+                if ('.' != (board[i][j]) && rowSet.contains(String.valueOf(board[i][j]))) {
+                    return false;
+                } else {
+                    rowSet.add(String.valueOf(board[i][j]));
+                }
+            }
+        }
 
 
-        return true;
+        return sodoku;
     }
 }
